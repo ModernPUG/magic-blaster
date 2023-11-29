3d7ce404f70c0fa558f9b2127bd972cbd027cb7f
Terrorboy <z9n@kakao.com>
2023-11-29 15:17:31 +0900

    add 'Terrorboy' User

---
+++ lib/Users/Terrorboy.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Users;
+
+class Terrorboy extends SampleUser
+{
+    public function getName(): string
+    {
+        // 요즘 바빠서 참여만 합니다! 파이팅!
+        return 'Terrorboy';
+    }
+}
